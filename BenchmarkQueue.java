01197dd669ee5aa3685ee4ab13c6c5848261c04c
Skinken <guma.moritz@gmail.com>
2024-10-01 11:59:28 +0200

    First assignment alsmot complete

---
+++ BenchmarkQueue.java
@@ -0,0 +1,5 @@
+public class BenchmarkQueue {
+    public static void main(String[] args){
+
+    }
+}
---
+++ LinkedListQueueBetter.java
@@ -0,0 +1,57 @@
+public class LinkedListQueueBetter{
+    Node head;
+    Node tail;
+
+    private class Node{
+        Integer item;
+        Node next;
+
+        private Node(Integer item, Node list){
+            this.item = item;
+            this.next = list;
+        } 
+    }
+
+    /*
+     * Constructor of the queue, creating instances of the queue
+     */
+    public LinkedListQueueBetter(){
+        head = null;
+        tail = null;
+    }
+
+    /*
+     * Adds a new item to the beginning of the queue
+     * The last item in the queue is the one that will be removed first, first in first out principle
+     * 
+     * @param item, is the item to be added to the list
+     */
+    public void enqueue(Integer item){
+        Node newNode = new Node(item, null);
+        if (head == null){
+            head = newNode; 
+            tail = newNode;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    /*
+     * Removes the item at the end of the queue and returns it
+     */
+    public Integer dequeue(){
+        if (head == null) return null;
+
+        Node current = head;
+        Node previous = null;
+        while(current.next != null){
+            previous = current;
+            current = current.next;
+        }
+
+        if (previous == null) head = null;
+        else previous.next = null;
+
+        return current.item;
+    }
+}
